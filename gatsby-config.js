7fdfe0a98000a09d8ba24ddcfcffa50beb038199
Mateie <matemachitidze2001@gmail.com>
2023-01-05 11:31:41 -0800

    Updated website description

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
   siteMetadata: {
     title: "Mate Machitidze",
     description:
-      "Mate Machitidze is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.",
+      "Mate Machitidze is a full-stack software engineer who builds and makes apps for the internet industry.",
     siteUrl: "https://mateie.me", // No trailing slash allowed!
     image: "/og.png", // Path to your image you placed in the 'static' folder
     twitterUsername: "@mateie2",
